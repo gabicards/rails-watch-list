42bd1c9b6ce4fe6389e359104fd7da45393c3360
Gabriela Cardoso <157655310+gabicards@users.noreply.github.com>
2024-05-20 09:38:33 +0200

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :movies, only: [:index, :show, :new, :create]
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
+    resources :bookmarks, only: [:new, :create]
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :bookmarks, only: [:destroy]
+
+  root 'lists#index'
 end
