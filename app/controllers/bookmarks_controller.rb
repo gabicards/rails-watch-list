21dec7bc69bc85836250da9b327f8fd590dc243d
Gabriela Cardoso <157655310+gabicards@users.noreply.github.com>
2024-05-20 09:54:04 +0200

    Bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,32 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = @list.bookmarks.build(bookmark_params)
+    if @bookmark.save
+      redirect_to @list, notice: 'Bookmark was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
 end
